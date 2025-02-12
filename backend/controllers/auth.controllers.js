3f7fa8a75114a65a294a651456ca20eb04d8ef26
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-12 16:41:40 -0600

    Get Users for Sidebar Endpoint ready

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -49,7 +49,7 @@ export const signup = async (req, res) => {
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
-        res.status(500).json({error: "Interna Server Error"});
+        res.status(500).json({error: "Internal Server Error"});
     }
 };
 
