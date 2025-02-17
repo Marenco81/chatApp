21caf131b81eff0d6784d62da86d59abec1e4f87
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-17 12:50:00 -0600

    Signup Page UI ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Login from './pages/login/login'
+import SignUp from './pages/signup/SignUp'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login>
-
-      </Login>
+      {/* <Login></Login> */}
+      <SignUp></SignUp>
     </div>
   )
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-blur-lg bg-opacity-0" >
-        <h1 className="text-3xl font-semibold text-center text-gray-500">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
            Login 
            <span className="text-blue-500"> ChatApp</span>
         </h1>
@@ -11,14 +11,14 @@ const Login = () => {
         <form>
             <div>
                 <label className="label p-2">
-                    <span className="text-base label-text text-gray-500">Username</span>
+                    <span className="text-base label-text text-gray-300">Username</span>
                 </label>
                 <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10 bg-gray-100" />
             </div>
 
             <div>
                 <label className="label">
-                    <span className="text-base label-text text-gray-500">Password</span>
+                    <span className="text-base label-text text-gray-300">Password</span>
                 </label>
                 <input 
                     type="password"
@@ -27,11 +27,13 @@ const Login = () => {
                 />
             </div>
 
-            <a href="#" className="text-sm text-gray-500 hover:underline hover:text-blue-600 mt-2 inline-block">{"Don't"} have an account?</a>
 
             <div>
-                <button className="btn btn-block btn-sm mt-2">Login</button>
+                <button className="btn btn-block btn-sm mt-8">Login</button>
             </div>
+
+            <a href="#" className="text-sm text-gray-300 hover:underline hover:text-blue-600 mt-4 flex justify-end">{"Don't"} have an account?</a>
+
         </form>
       </div>
     </div>
