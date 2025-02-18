8fd5f1caf4f49fa90b72b3f27b7761f0673ae80a
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-18 14:26:31 -0600

    Message Input for MessageContainer

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,9 +1,11 @@
 
 const MessageInput = () => {
   return (
-    <div>
-      
-    </div>
+    <form className="px-4 my-3">
+      <div className="w-full">
+        <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-200 text-gray-800" placeholder="Type your message"/>
+      </div>
+    </form>
   )
 }
 
