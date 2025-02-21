bffee7027537ff7e74871384db973192a3093fc1
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-20 21:45:28 -0600

    Create Authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/login';
 import SignUp from './pages/signup/SignUp';
-import Toaster from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
 
+  const {authUser} = useAuthContext();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Login></Login> */}
-      {/* <SignUp></SignUp> */}
-      {/* <Home></Home> */}
-      <Toaster></Toaster>
+      
       <Routes>
-        <Route path='/' element={<Home></Home>} />
-        <Route path='/login' element={<Login></Login>} />
-        <Route path='/signup' element={<SignUp></SignUp>} />
+        <Route path='/' element={authUser ? <Home></Home> : <Navigate to="/login" />} />
+        <Route path='/login' element={authUser ?  <Navigate to="/" /> : <Login></Login>} />
+        <Route path='/signup' element={authUser ?  <Navigate to="/" /> : <SignUp></SignUp>} />
       </Routes>
+      <Toaster/>
     </div>
   )
 }
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const {setAuthUser} = useAuthContext()
 
   const signup = async ({fullName, username, password, confirmPassword, gender}) => {
     const success = handleInputErrors({fullName, username, password, confirmPassword, gender})
@@ -15,10 +17,16 @@ const useSignup = () => {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({fullName, username, password, confirmPassword, gender})
-        })
+        });
 
         const data = await res.json();
-        console.log(data);
+        if(data.error) {
+            throw new Error(data.error)
+        }
+        // local storage
+        localStorage.setItem("chat-user", JSON.stringify(data))
+        // context
+        setAuthUser(data)
 
     } catch (error) {
       toast.error(error.message)  
