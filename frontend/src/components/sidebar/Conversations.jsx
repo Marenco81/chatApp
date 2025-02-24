673e0d776b5f8afb637827b2939c940a427abb59
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-24 12:16:59 -0600

    Get conversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,28 @@
+import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation"
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversations();
+  console.log('CONVERSATIONS: ', conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
+
+      {conversations.map((conversation, idx) => (
+        <Conversation 
+          key={conversation._id}
+          conversation={conversation}
+          lastIdx={idx === conversations.length - 1}
+        />  
+      ))}
+
+      {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
+      
+      {/* <Conversation></Conversation>
       <Conversation></Conversation>
       <Conversation></Conversation>
       <Conversation></Conversation>
       <Conversation></Conversation>
-      <Conversation></Conversation>
-      <Conversation></Conversation>
+      <Conversation></Conversation> */}
     </div>
   )
 }
