fbfa9932e8a3bee8529f6d79ece3646e788c6230
Alvaro Marenco <alvaromarencom@gmail.com>
2025-03-05 15:05:11 -0600

    New message notification animation and sound

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -27,13 +28,21 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        // SOCKET IO functionality will go here
 
         // await conversation.save();
         // await newMessage.save();
 
         //This will run in parallel (both will run at the same time)
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        // SOCKET IO functionality will go here
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            // io.to(<socket_id>).emt() used to send events to a specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+            
+        }
+
         res.status(201).json(newMessage);
 
     } catch (error) {
