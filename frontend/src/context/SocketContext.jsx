8d2fd9abd47bffa6af6440cb3d34158436f8a508
Alvaro Marenco <alvaromarencom@gmail.com>
2025-03-08 11:04:41 -0600

    Socket URL updated on SocketContext

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatapp-production-wnvy.onrender.com", { // http://localhost:5000" (for development)
                 query: {
                     userId: authUser._id,
                 },
